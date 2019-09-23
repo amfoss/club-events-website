e23e0b9e98d7b8fce4dfd7382c53e0635438d33d
Ashwin Shenoy <aswinshenoy65@gmail.com>
2019-09-23 17:01:20 +0530

    Initialize Gatsby Project

---
+++ README.md
@@ -0,0 +1,2 @@
+# MLH Local Hack Day - Landing Page
+Landing page for the MLH Local Hack Day: Learn organized by amFOSS.
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="MLH Local Hack Day" />
+    <h1>MLH Local Hack Day</h1>
+  </Layout>
+)
+
+export default IndexPage
