4c926e18933c889c77cb1ffbcfef9b7bf92aa858
harshithpabbati <pabbatiharshith@gmail.com>
2019-09-29 16:08:03 +0530

    Remove Checkbox

--- src/events/hacktoberfest/components/registration.js
+++ src/events/hacktoberfest/components/registration.js
@@ -29,15 +29,12 @@ class Registration extends React.Component {
       errorText: '',
       successText: '',
       loading: false,
-      agreed: false,
-      verified: true //change it to false when captcha is enabled
     }
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handlePhoneChange = this.handlePhoneChange.bind(this);
     this.handleGenderChange = this.handleGenderChange.bind(this);
     this.handleRollChange = this.handleRollChange.bind(this);
-    this.handleCheckboxChange = this.handleCheckboxChange.bind(this);
   }
 
   handleNameChange(event) {
@@ -55,10 +52,6 @@ class Registration extends React.Component {
   handleRollChange(event) {
     this.setState({ roll: event.target.value });
   }
-  handleCheckboxChange(event) {
-    const prev = this.state.agreed
-    this.setState( { agreed: !prev});
-  }
 
   register = async() => {
     const { name, email, phone, gender, roll, agreed, verified } = this.state;
@@ -73,10 +66,6 @@ class Registration extends React.Component {
       this.setState({ loading: false, errorText: "Enter Proper Phone" })
     } else if (rollRegex.test(roll.toUpperCase()) === false) {
       this.setState({ loading: false, errorText: "Enter Amrita Roll Number in proper format - AM.EN.U4XXX00000" })
-    } else if (!agreed) {
-      this.setState({ loading: false, errorText: "Please agree to COC & Privacy Policy." })
-    } else if (!verified){
-      this.setState({ loading: false, errorText: "Please complete the ReCaptcha. This helps us to fight spams." })
     } else {
       const json = { 'gender': gender, "rollNo": roll }
       const variables = { name, email, phone, formData: JSON.stringify(json) }
@@ -154,21 +143,9 @@ class Registration extends React.Component {
                     </div>
                     <div className="col-12 form-check">
                       <div className="m-2 text-light d-flex justify-content-center">
-                        <div className="row m-0">
-                          <div className="col-sm-1 col-2 d-flex justify-content-center align-items-center">
-                            <input
-                                  type="checkbox"
-                                  className="form-check-input"
-                                  id="undertaking"
-                                  onChange={() => this.handleCheckboxChange}
-                            />
-                          </div>
-                          <div className="col">
-                            <label className="form-check-label" htmlFor="undertaking">
-                              By submitting this application, I agree to the <a href="#">Code of Conduct</a> & <a href="#">Privacy Policy</a> of the organizers.
-                            </label>
-                          </div>
-                        </div>
+                        <label className="form-check-label" htmlFor="undertaking">
+                          By submitting this application, I agree to the <a href="#">Code of Conduct</a> & <a href="#">Privacy Policy</a> of the organizers.
+                        </label>
                       </div>
                     </div>
                     {
@@ -191,7 +168,7 @@ class Registration extends React.Component {
                 </form>
                 </div>) : this.state.successText !== '' ? (<div className="alert alert-success">
                 Thank You! You have successfully signed up for the event.
-                See you at event, dont forget to bring your friends as well!
+                See you at event, and dont forget to bring your friends as well!
                 </div>) :
                 <div className="alert alert-warning">Submitting. Please Wait</div>
               }
