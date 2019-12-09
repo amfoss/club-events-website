7b4a35f33f4c72a51a858e5555b1880e53e38bc7
harshithpabbati <pabbatiharshith@gmail.com>
2019-12-09 19:57:15 +0530

    schedule: Update the timeline of mlh build

--- src/events/mlh-build-day/components/schedule.js
+++ src/events/mlh-build-day/components/schedule.js
@@ -6,7 +6,7 @@ const Schedule = () => {
       <h2 className="text-center pt-4">Event Schedule</h2>
       <div className="row mt-4 p-4">
         <div className="col-md-1"/>
-        <div className="col-md-5 col-sm-12">
+        <div className="col-md-10 col-sm-12">
           <table className="table lead table-hover text-center">
             <thead>
             <tr>
@@ -16,60 +16,32 @@ const Schedule = () => {
             </thead>
             <tbody>
             <tr>
-              <td>08:00AM</td>
+              <td>08:30AM - 09:00AM</td>
               <td>Arrival and Check-in</td>
             </tr>
             <tr>
-              <td>09:00AM</td>
-              <td>Presentation about MLH!</td>
+              <td>09:00AM - 09:15AM</td>
+              <td>Introduction to hackathon, problems released</td>
             </tr>
             <tr>
               <td>9:15AM</td>
-              <td>Workshop on "Add an FAQ Bot to Your Webpage with Microsoft Azure"</td>
+              <td>Hackathon starts</td>
             </tr>
             <tr>
-              <td>10:45AM</td>
-              <td>Clearing any doubts</td>
+              <td>12:00PM - 2:00PM</td>
+              <td>Leave for lunch at anytime between 12 and 2</td>
             </tr>
             <tr>
-              <td>11:00AM</td>
-              <td>Introduction to Hackathon,Problem Statements released</td>
-            </tr>
-            </tbody>
-          </table>
-        </div>
-        <div className="col-md-5">
-          <table className="table lead table-hover text-center">
-            <thead>
-            <tr>
-              <th className="text-center">Time</th>
-              <th className="text-center">Event</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr>
-              <td>11:15AM</td>
-              <td>Hack Begins.</td>
+              <td>02:00PM - 05:30PM</td>
+              <td>Hackathon continues</td>
             </tr>
             <tr>
-              <td>12:00PM</td>
-              <td>Lunch</td>
+              <td>05:30PM</td>
+              <td>Hackathon ends</td>
             </tr>
             <tr>
-              <td>3:00PM</td>
-              <td>A fun activity</td>
-            </tr>
-            <tr>
-              <td>8:30PM</td>
-              <td>Hack Ends!</td>
-            </tr>
-            <tr>
-              <td>8:30PM</td>
-              <td>Final Pitching session. </td>
-            </tr>
-            <tr>
-              <td>9:30PM</td>
-              <td>Awards and closing ceremony</td>
+              <td>5:30PM - 6:00PM</td>
+              <td>Results and swag distribution</td>
             </tr>
             </tbody>
           </table>
